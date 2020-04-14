8d1902fb7f98062f213c5ffce181258845c5197d
Yeonggyun.Kim <kygha7205@naver.com>
2020-04-15 03:54:40 +0900

    Add cv

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img alt="logo" src="./assets/photo.jpg" width="200">
+    <HelloWorld msg="Yeonggyun Kim"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/content.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap");
+@import url("https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css");
+@import url('https://fonts.googleapis.com/css?family=Raleway:400,700,800&display=swap');
+
+#app {
+  font-family: "Noto Sans KR", Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  width: 60%;
+  color: #2c3e50;
+  text-align: center;
+  margin: 60px auto 5em;
+}
+  img {
+    border-radius: 50%;
+  }
+</style>
---
+++ src/components/content.vue
@@ -0,0 +1,299 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <h2>Software Engineer</h2>
+    <p>
+      Seoul, South Korea.
+    </p>
+    <div class="flex-box top-link-container">
+      <span><a href="mailto:0.gyun@zerogyun.dev" target="_blank" class="top-link"><i class="fa fa-envelope"></i> 0.gyun@zerogyun.dev</a></span>
+      <span class="divider" style="width: 1px;">|</span>
+      <span><a href="https://github.com/cxz7720" target="_blank" class="top-link"><i class="fa fa-github"></i> CXZ7720</a> </span>
+      <span class="divider" style="width: 1px;">|</span>
+      <span><a href="www.linkedin.com/in/zerogyun" target="_blank" class="top-link"><i class="fa fa-linkedin-square"></i> zerogyun</a> </span>
+      <span class="divider" style="width: 1px;">|</span>
+      <span><a href="https://cutt.ly/zerogyun_cv" target="_blank" class="top-link"><i class="fa fa-file-text-o"></i> Downlod CV</a> </span>
+
+    </div>
+    <p>
+      Fullstack Web developer, Flutter developer.<br />
+      Love to cooperatie with people to solve problem.
+    </p>
+    <div class="content-area">
+      <h3 class="title">Education</h3>
+      <p class="sub_title">
+        B.S, Computer Science and Engineering
+      </p>
+      <p class="normal_text flex-box">
+        <span class="flex-grow1">Hanyang University ERICA</span>
+        <span class="due_text">2015.03 ~ Present</span>
+      </p>
+
+      <h3 class="title">Experiences</h3>
+      <div class="content-group">
+        <p class="sub_title">
+          Coinone. Inc <a href="https://coinone.co.kr" target="_blank"><i class="fa fa-external-link icon"></i></a>
+        </p>
+        <p class="normal_text flex-box">
+          <span class="flex-grow1 role">Technical Intern</span>
+          <span class="due_text">2018.08</span>
+        </p>
+        <ul>
+          <li>
+            Industry-University Cooperation project - Personal trading portfolio
+          </li>
+        </ul>
+      </div>
+      <div class="content-group">
+        <p class="sub_title">
+          R.O.K.A, Defense Security Agency
+        </p>
+        <p class="normal_text flex-box">
+          <span class="flex-grow1 role">Full Stack Web developer</span>
+          <span class="due_text">2016.05 ~ 2018.02</span>
+        </p>
+        <ul>
+          <li>
+            Web Full stack developer(Javascipt, PHP)
+          </li>
+          <li>
+            CERT member
+          </li>
+          <li>
+            Served and discharged from ROK Army as SW Developer, Sergeant
+          </li>
+        </ul>
+      </div>
+      <h3 class="title">Projects</h3>
+      <div class="content-group">
+        <p class="sub_title">
+          Bus Hanyang, 버스하냥 <a href="https://hubus.app" target="_blank"><i class="fa fa-external-link icon"></i></a>
+        </p>
+        <p class="normal_text flex-box">
+          <span class="flex-grow1 role">Project Manager, Frontend developer, Backend API developer, DevOps developer</span>
+          <span class="due_text">2019.09 ~ 2020.03</span>
+        </p>
+        <ul>
+          <li>
+            University shuttle bus arrival information service(PWA WEB).
+          </li>
+          <li>
+            Application API Server(Node.js), Frontend(Vue.js)
+          </li>
+          <li>
+            CI/CD(Jenkins, Google App Engine, Google Cloud Builder, Docker)
+          </li>
+        </ul>
+      </div>
+      <div class="content-group">
+        <p class="sub_title">
+          HYBUS <a href="https://play.google.com/store/apps/details?id=com.busHanyang.hybus" target="_blank"><i class="fa fa-external-link icon"></i></a>
+        </p>
+        <p class="normal_text flex-box">
+          <span class="flex-grow1 role">Solo Project</span>
+          <span class="due_text">2019.09 ~ 2019.12</span>
+        </p>
+        <ul>
+          <li>
+            University shuttle bus arrival information service Android application.
+          </li>
+          <li>
+            Android Application(Flutter)
+          </li>
+        </ul>
+      </div>
+      <h3 class="title">Hornor and Awards</h3>
+      <div class="content-group">
+        <p class="sub_title">
+          Hanyang University SW/ICT Symposium<a href="https://hubus.app" target="_blank"><i class="fa fa-external-link icon"></i></a>
+        </p>
+        <p class="normal_text flex-box">
+          <span class="flex-grow1 role">Industry-Academic cooperation branch -  3rd position</span>
+          <span class="due_text">2019.11</span>
+        </p>
+      </div>
+      <div class="content-group">
+        <p class="sub_title">
+          Hanyang University SW/ICT Symposium<a href="https://hubus.app" target="_blank"><i class="fa fa-external-link icon"></i></a>
+        </p>
+        <p class="normal_text flex-box">
+          <span class="flex-grow1 role">Venture Startup branch -  3rd position</span>
+          <span class="due_text">2019.11</span>
+        </p>
+      </div>
+      <h3 class="title">Publications</h3>
+      <div class="content-group">
+        <p class="sub_title">
+          Automated Smart Plug Manager<a href="http://www.dbpia.co.kr/journal/articleDetail?nodeId=NODE07503513" target="_blank"><i class="fa fa-external-link icon"></i></a>
+        </p>
+        <p class="normal_text flex-box">
+          <span class="flex-grow1 role">Jinsoo Han, Jino Park, Yeonggyun Kim, Sunghyun Cho</span>
+          <span class="due_text">2018.06</span>
+        </p>
+        <ul>
+          <li>
+            The Korean Institute of Information Scientists and Engineers, 한국정보과학회 학술발표논문집,<br>1834-1836(3 pages)
+          </li>
+        </ul>
+      </div>
+      <h3 class="title">Organizations</h3>
+      <div class="content-group">
+        <p class="sub_title">
+          JARAM<a href="https://jaram.net" target="_blank"><i class="fa fa-external-link icon"></i></a>
+        </p>
+        <p class="normal_text flex-box">
+          <span class="flex-grow1 role">Member</span>
+          <span class="due_text">2016.09 ~ Present</span>
+        </p>
+        <ul>
+          <li>
+            <i>JARAM</i> is an academic club in College of Computing.
+          </li>
+        </ul>
+      </div>
+      <div class="content-group">
+        <p class="sub_title">
+          Developer Students Clubs <i>Hanyang Univ. ERICA </i><a href="https://developers.google.com/community/dsc" target="_blank"><i class="fa fa-external-link icon"></i></a>
+        </p>
+        <p class="normal_text flex-box">
+          <span class="flex-grow1 role">Member</span>
+          <span class="due_text">2019.09 ~ Present</span>
+        </p>
+      </div>
+    </div>
+    <footer>
+      <div class="container content-area flex-box">
+        <span class="">© 2020 Yeonggyun Kim</span>
+        <span>
+          <a href="https://cutt.ly/zerogyun_cv" target="_blank">Download CV</a>
+          <span class="divider" style="width: 1px;">|</span>
+          <a href="https://zerogyun.dev" target="_blank">Blog</a>
+        </span>
+
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.content-area {
+  margin: auto;
+  text-align: start;
+}
+
+h1 {
+  font-size: 2.5em;
+  font-weight: 800;
+  font-family: "Raleway","Noto Sans KR",serif;
+  margin: 0.2em 0 0 0;
+}
+h2 {
+  margin: 0.2em 0 0 0;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  color: ;
+  list-style-type: square;
+  padding: 0;
+  margin: 0;
+}
+li {
+  display: list-item;
+  margin-left: 1em;
+}
+a {
+  text-decoration: none;
+}
+
+
+p {
+  margin: 0;
+}
+
+footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: #f5f5f5;
+}
+.top-link-container {
+  justify-content: center;
+  font-size: 1em;
+  border-bottom: solid 1px #b4b4b4;
+  padding-bottom: 1em;
+  margin-bottom: 1em;
+}
+
+.top-link {
+  color: rgb(79, 144, 205);
+}
+
+.title {
+  color: rgb(79, 144, 205);
+  font-size: 1.8em;
+  font-weight: bold;
+}
+
+.sub_title {
+  font-size: 1.2em;
+  color: black;
+  font-weight: bold;
+}
+
+.normal_text {
+  color: black;
+  font-weight: normal;
+}
+
+.flex-box {
+  display: flex !important;
+}
+
+.flex-grow1 {
+  flex-grow: 1 !important;
+}
+
+.due_text {
+  color: rgb(79, 144, 205);
+  font-style: italic;
+}
+
+.content-group {
+  margin-bottom: 1em;
+}
+
+.role {
+  font-style: italic;
+}
+
+  .icon {
+    font-size: 0.7em;
+    margin-left: 0.5em;
+    color: rgb(79, 144, 205);
+  }
+
+  .container {
+
+    width: 60%;
+    height: 3em;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .divider {
+    margin: 0 0.5em
+  }
+</style>
